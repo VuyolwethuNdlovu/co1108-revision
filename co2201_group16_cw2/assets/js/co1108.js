c8e70b13af4921e080e5135a3063abe052a9494b
VuyoNdlovu <vnn3@student.le.ac.uk>
2021-09-10 18:21:47 +0100

    Uploaded CO1108 Revision Project

---
+++ co2201_group16_cw2/assets/js/co1108.js
@@ -0,0 +1,15 @@
+document.getElementById("showAnswer").onclick = function() {
+    document.getElementById("answer").style.visibility = "visible";
+}
+
+document.getElementById("showExplanation").onclick = function() {
+    document.getElementById("explanation").style.visibility = "visible";
+}
+
+document.getElementById("hideAnswer").onclick = function() {
+    document.getElementById("answer").style.visibility = "hidden";
+}
+
+document.getElementById("hideExplanation").onclick = function() {
+    document.getElementById("explanation").style.visibility = "hidden";
+}
